972f080c7f0844a710a690454534d7d5c8436278
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2023-01-21 00:51:28 +0100

    Modified SignInPage.tsx

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -6,13 +6,13 @@ import { useNavigate } from 'react-router-dom';
 export function SignInPage(): JSX.Element {
     const navigate = useNavigate();
     return (
-            <SignInForm
-                projectId="f85d61a5-2f6d-49bb-a2ec-8a95abb55a86"
-                googleClientId="397236612778-c0b5tnjv98frbo1tfuuha5vkme3cmq4s.apps.googleusercontent.com"
-                onSuccess={() => navigate('/')}
-                >
-                <Logo size={32} />
-                <Text size="lg">Sign in to Medplum Demo App</Text>
-            </SignInForm>
-            );
+        <SignInForm
+            //projectId="7bf160a1-b32b-4d6d-8980-1de0a8020a0d"
+            googleClientId="397236612778-c0b5tnjv98frbo1tfuuha5vkme3cmq4s.apps.googleusercontent.com"
+            onSuccess={() => navigate('/')}
+            >
+            <Logo size={32} />
+            <Text size="lg">Sign in to Medplum Demo App</Text>
+        </SignInForm>
+    );
 }
